e37b6c19db9f19e43a8c8a3103acef1fd86b9f33
asus <asus@DESKTOP-5V94U0C>
2020-05-06 16:39:47 +0530

    ServletProj-01-DateApp

---
+++ ServletProj_01_DateApp/src/main/java/com/nt/servlet/DateServlet.java
@@ -0,0 +1,31 @@
+package com.nt.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class DateServlet extends GenericServlet{
+
+	@Override
+	public void service(ServletRequest req, ServletResponse resp) throws ServletException, IOException {
+		PrintWriter pw=null;
+		Date d=null;
+		
+		//Set Response typecontent
+		resp.setContentType("text/html");
+		//Get PrintWriter
+		pw=resp.getWriter();
+		//Write b logic
+		d=new Date();
+		pw.println("<h1 style='color:red;text-align:center;'>Date And Time::"+d+"</h1>");
+		//close stream
+		pw.close();
+		
+	}
+
+}
